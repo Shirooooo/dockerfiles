36f1efe2af1f87774bff7b9181cf9ba3e9d9e118
qwinwin <qwinwin7@gmail.com>
2021-03-08 18:30:44 +0800

    update

--- ss_privoxy/docker-entrypoint.sh
+++ ss_privoxy/docker-entrypoint.sh
@@ -4,7 +4,7 @@ if [[ "$SS_PORT" ]]; then
     echo "forward-socks5 / 0.0.0.0:${SS_PORT} ." >>/etc/privoxy/config
 fi
 if [[ "$PRIVOXY_PORT" ]]; then
-    sed -i '/^listen-address/d' /etc/privoxy/config && \ 
+    sed -i '/^listen-address/d' /etc/privoxy/config
     echo "listen-address  0.0.0.0:${PRIVOXY_PORT}" >>/etc/privoxy/config
 fi
 
